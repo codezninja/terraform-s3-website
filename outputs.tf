c714d0c0008903e7bf02c55b706479afc26307d4
Ezbon Jacob <ezjacob@gmail.com>
2019-03-05 18:45:48 -0500

    added hosted_zone_id for main_bucket

--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,3 @@
 output "main_bucket" { value = "${aws_s3_bucket.main_bucket.bucket}" }
 output "main_bucket_website_endpoint" { value = "${aws_s3_bucket.main_bucket.website_endpoint}" }
+output "main_bucket_hosted_zone_id" { value = "${aws_s3_bucket.main_bucket.hosted_zone_id}" }
