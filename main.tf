a072876ff74559132ef6970c8d36d5e6eaa93c33
Ezbon Jacob <ezjacob@gmail.com>
2019-03-05 21:46:59 -0500

    allow get object public

--- main.tf
+++ main.tf
@@ -1,6 +1,24 @@
 resource "aws_s3_bucket" "main_bucket" {
   bucket = "www.${var.site}.com"
   acl    = "public-read"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AddPerm",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::www.$${var.site}.com/*"
+            ]
+        }
+    ]
+}
+EOF
 
   website {
     index_document = "index.html"
