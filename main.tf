7075fd359beb70536328320702d46b78b8b15fdd
Ezbon Jacob <ezjacob@gmail.com>
2019-03-11 21:46:25 -0400

    fixed policy for staging

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
-data "template_file" "s3_public_read" {
+data "template_file" "s3_public_read_www" {
   template = "${file("${path.module}/policy.json")}"
   vars {
     site = "${var.site}"
+    cname = "www"
   }
 }
 
 resource "aws_s3_bucket" "main_bucket" {
   bucket = "www.${var.site}.com"
   acl    = "public-read"
-  policy = "${data.template_file.s3_public_read.rendered}"
+  policy = "${data.template_file.s3_public_read_www.rendered}"
 
   website {
     index_document = "index.html"
@@ -16,10 +17,18 @@ resource "aws_s3_bucket" "main_bucket" {
   }
 }
 
+data "template_file" "s3_public_read_staging" {
+  template = "${file("${path.module}/policy.json")}"
+  vars {
+    site = "${var.site}"
+    cname = "staging"
+  }
+}
+
 resource "aws_s3_bucket" "staging_bucket" {
   bucket = "staging.${var.site}.com"
   acl    = "public-read"
-  policy = "${data.template_file.s3_public_read.rendered}"
+  policy = "${data.template_file.s3_public_read_staging.rendered}"
 
   website {
     index_document = "index.html"
